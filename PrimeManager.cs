7c29809ca065815e3162cdc44d3a6c3319357c58
TTerry8155 <tterry8155@gmail.com>
2017-07-25 14:57:13 -0500

    Initialize and Complete

---
+++ PrimeManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DetermineIfANumberIsPrime
+{
+    public class PrimeManager
+    {
+        public static bool CheckIfPrime(int number)
+        {
+            return (Enumerable.Range(1, number).Count(x => number % x == 0) == 2);
+        }
+    }
+}
